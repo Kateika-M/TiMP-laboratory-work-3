497c0f26f0ba50eb929557afa99a16750d6122b1
Kateika-M <muromskaya.katya@mail.ru>
2021-05-28 22:57:24 +0300

    tasks

---
+++ timp3_2/cmake-build-debug/CMakeFiles/timp3_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/timp3_2.dir/main.cpp.o"
+  "timp3_2"
+  "timp3_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/timp3_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
